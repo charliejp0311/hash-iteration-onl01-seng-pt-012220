40f1479dad0cf331eec5599ac4091bda639be322
Charles Pelton <charliejp0311@gmail.com>
2020-01-28 15:21:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,11 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |key, value|
+    if value < 12
+      puts "Happy Birthday #{key}! You are now #{value} years old!"  
+    end
+  end
+end
